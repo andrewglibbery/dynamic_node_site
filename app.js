1aece706b47f39729a73d62a225290bed1c9cc80
Andrew Glibbery <andrew.glibbery@gmail.com>
2016-06-17 12:18:29 -0400

    gets json profile info

---
+++ app.js
@@ -0,0 +1,19 @@
+var router = require("./router.js");
+//problem: we need a simple way to look at a users badge count
+//solution: use node.js to perform the profile look ups and server our template via http
+
+
+//Create a web server
+
+var http = require('http');
+http.createServer(function (request, response) {
+	router.home(request, response);
+	router.user(request, response);
+}).listen(1337, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
+
+
+
+//Function that handles the reading of files and merge in value
+	//read from file and get a string
+		//merge values in to a string
